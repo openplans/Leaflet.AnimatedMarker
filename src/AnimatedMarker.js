755b56e8145f72a733fb7ef71cfc3246f325fb6f
Aaron Ogle <atogle@gmail.com>
2026-01-20 11:50:46 -0500

    Modernize AnimatedMarker.js and fix zoom bug
    - Convert to ES6 syntax (const/let, arrow functions)
    - Add UMD wrapper for AMD/CommonJS/browser support
    - Fix zoom bug: disable CSS transitions during zoom to prevent
      marker floating to new position
    - Remove legacy _chunk() fallback for non-CSS3 browsers
    - Rename 'clickable' option to 'interactive' (Leaflet 1.x naming)
    - Add proper cleanup in onRemove()

--- src/AnimatedMarker.js
+++ src/AnimatedMarker.js
@@ -1,117 +1,142 @@
-L.AnimatedMarker = L.Marker.extend({
-  options: {
-    // meters
-    distance: 200,
-    // ms
-    interval: 1000,
-    // animate on add?
-    autoStart: true,
-    // callback onend
-    onEnd: function(){},
-    clickable: false
-  },
+/**
+ * Leaflet.AnimatedMarker
+ * Animate a marker along a polyline.
+ *
+ * https://github.com/openplans/Leaflet.AnimatedMarker
+ * License: MIT
+ */
 
-  initialize: function (latlngs, options) {
-    this.setLine(latlngs);
-    L.Marker.prototype.initialize.call(this, latlngs[0], options);
-  },
-
-  // Breaks the line up into tiny chunks (see options) ONLY if CSS3 animations
-  // are not supported.
-  _chunk: function(latlngs) {
-    var i,
-        len = latlngs.length,
-        chunkedLatLngs = [];
-
-    for (i=1;i<len;i++) {
-      var cur = latlngs[i-1],
-          next = latlngs[i],
-          dist = cur.distanceTo(next),
-          factor = this.options.distance / dist,
-          dLat = factor * (next.lat - cur.lat),
-          dLng = factor * (next.lng - cur.lng);
-
-      if (dist > this.options.distance) {
-        while (dist > this.options.distance) {
-          cur = new L.LatLng(cur.lat + dLat, cur.lng + dLng);
-          dist = cur.distanceTo(next);
-          chunkedLatLngs.push(cur);
-        }
-      } else {
-        chunkedLatLngs.push(cur);
-      }
-    }
-    chunkedLatLngs.push(latlngs[len-1]);
-
-    return chunkedLatLngs;
-  },
-
-  onAdd: function (map) {
-    L.Marker.prototype.onAdd.call(this, map);
-
-    // Start animating when added to the map
-    if (this.options.autoStart) {
-      this.start();
-    }
-  },
-
-  animate: function() {
-    var self = this,
-        len = this._latlngs.length,
-        speed = this.options.interval;
-
-    // Normalize the transition speed from vertex to vertex
-    if (this._i < len && this._i > 0) {
-      speed = this._latlngs[this._i-1].distanceTo(this._latlngs[this._i]) / this.options.distance * this.options.interval;
-    }
-
-    // Only if CSS3 transitions are supported
-    if (L.DomUtil.TRANSITION) {
-      if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = ('all ' + speed + 'ms linear'); }
-      if (this._shadow) { this._shadow.style[L.DomUtil.TRANSITION] = 'all ' + speed + 'ms linear'; }
-    }
-
-    // Move to the next vertex
-    this.setLatLng(this._latlngs[this._i]);
-    this._i++;
-
-    // Queue up the animation to the next next vertex
-    this._tid = setTimeout(function(){
-      if (self._i === len) {
-        self.options.onEnd.apply(self, Array.prototype.slice.call(arguments));
-      } else {
-        self.animate();
-      }
-    }, speed);
-  },
-
-  // Start the animation
-  start: function() {
-    this.animate();
-  },
-
-  // Stop the animation in place
-  stop: function() {
-    if (this._tid) {
-      clearTimeout(this._tid);
-    }
-  },
-
-  setLine: function(latlngs){
-    if (L.DomUtil.TRANSITION) {
-      // No need to to check up the line if we can animate using CSS3
-      this._latlngs = latlngs;
-    } else {
-      // Chunk up the lines into options.distance bits
-      this._latlngs = this._chunk(latlngs);
-      this.options.distance = 10;
-      this.options.interval = 30;
-    }
-    this._i = 0;
+(function (factory) {
+  // UMD wrapper
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['leaflet'], factory);
+  } else if (typeof module !== 'undefined' && module.exports) {
+    // Node/CommonJS
+    module.exports = factory(require('leaflet'));
+  } else {
+    // Browser globals
+    factory(window.L);
   }
+}(function (L) {
+  'use strict';
 
-});
+  L.AnimatedMarker = L.Marker.extend({
+    options: {
+      // meters per interval
+      distance: 200,
+      // milliseconds
+      interval: 1000,
+      // animate on add?
+      autoStart: true,
+      // callback on animation end
+      onEnd: function () {},
+      // marker is not interactive by default
+      interactive: false
+    },
 
-L.animatedMarker = function (latlngs, options) {
-  return new L.AnimatedMarker(latlngs, options);
-};
+    initialize: function (latlngs, options) {
+      this.setLine(latlngs);
+      L.Marker.prototype.initialize.call(this, latlngs[0], options);
+    },
+
+    onAdd: function (map) {
+      L.Marker.prototype.onAdd.call(this, map);
+
+      // Disable transition during zoom to prevent marker floating
+      this._map.on('zoomstart', this._disableTransition, this);
+      this._map.on('zoomend', this._enableTransition, this);
+
+      if (this.options.autoStart) {
+        this.start();
+      }
+    },
+
+    onRemove: function (map) {
+      // Clean up zoom event listeners
+      this._map.off('zoomstart', this._disableTransition, this);
+      this._map.off('zoomend', this._enableTransition, this);
+
+      this.stop();
+      L.Marker.prototype.onRemove.call(this, map);
+    },
+
+    animate: function () {
+      const len = this._latlngs.length;
+      let speed = this.options.interval;
+
+      // Normalize the transition speed from vertex to vertex
+      if (this._i < len && this._i > 0) {
+        speed = this._latlngs[this._i - 1].distanceTo(this._latlngs[this._i]) / this.options.distance * this.options.interval;
+      }
+
+      // Store current speed for re-enabling transition after zoom
+      this._currentSpeed = speed;
+
+      // Apply CSS transition for smooth animation
+      this._setTransition(speed);
+
+      // Move to the next vertex
+      this.setLatLng(this._latlngs[this._i]);
+      this._i++;
+
+      // Queue up the animation to the next vertex
+      this._tid = setTimeout(() => {
+        if (this._i === len) {
+          this.options.onEnd.call(this);
+        } else {
+          this.animate();
+        }
+      }, speed);
+    },
+
+    start: function () {
+      this._i = 0;
+      this.animate();
+    },
+
+    stop: function () {
+      if (this._tid) {
+        clearTimeout(this._tid);
+        this._tid = null;
+      }
+    },
+
+    setLine: function (latlngs) {
+      this._latlngs = latlngs;
+      this._i = 0;
+    },
+
+    _setTransition: function (speed) {
+      if (this._icon) {
+        this._icon.style.transition = 'all ' + speed + 'ms linear';
+      }
+      if (this._shadow) {
+        this._shadow.style.transition = 'all ' + speed + 'ms linear';
+      }
+    },
+
+    _disableTransition: function () {
+      if (this._icon) {
+        this._icon.style.transition = 'none';
+      }
+      if (this._shadow) {
+        this._shadow.style.transition = 'none';
+      }
+    },
+
+    _enableTransition: function () {
+      // Re-enable transition with current animation speed
+      if (this._currentSpeed) {
+        this._setTransition(this._currentSpeed);
+      }
+    }
+  });
+
+  // Factory function
+  L.animatedMarker = function (latlngs, options) {
+    return new L.AnimatedMarker(latlngs, options);
+  };
+
+  return L.AnimatedMarker;
+}));
